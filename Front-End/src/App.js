a80aa9d8ca963e7deac42eb2f8d6a5f374e295e8
lhirsch1 <hirschlukasa@gmail.com>
2021-10-14 12:56:53 -0500

    adding states and comments for future functions

--- Front-End/src/App.js
+++ Front-End/src/App.js
@@ -1,5 +1,17 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from "react"
 import './App.css';
+
+//store company in state
+
+const [companyState, setCompanyState] = useState()
+const [guestState, setGuestState] = useState()
+const [messageState, setMessageState] = useState()
+
+//function get companys/guests to populate cards
+
+
+//function call api to get user messages
+
 
 function App() {
   return (
@@ -8,7 +20,7 @@ function App() {
        <h1>Hotel Manager</h1>
       </header>
       <body>
-        
+
       </body>
 
     </div>
