26c18b7a2c9b11433714eedfb3ef0dcd5d82f21f
lhirsch1 <hirschlukasa@gmail.com>
2021-10-14 15:19:45 -0500

    wrapping in div

--- Front-End/src/Components/infoSilo.js
+++ Front-End/src/Components/infoSilo.js
@@ -1,53 +1,42 @@
 //for each object generates appropriate card
-import {CompanyCard} from "./companyCard"
-import {GuestCard} from "./guestCard"
+import { CompanyCard } from "./companyCard";
+import { GuestCard } from "./guestCard";
+import '../App.css';
 
-
-export const InfoSilo = ({allCompanies, allGuests, allMessages}) => {
-    
-    if(allCompanies && allCompanies.length >= 1){
-        console.log(' all companies ', allCompanies)
-        return (
-            <>
-                HI THERE
-                {allCompanies !== undefined ? (
-                    allCompanies.map((company,idx) =>{
-                        return <CompanyCard idx={idx} company={company} />
-                    })
-                ) :null 
-                }
-            </>
-        )
-    }
-    else if(allGuests && allGuests.length >= 1){
-        console.log('all guests ', allGuests)
-        return (
-            <>
-                {allGuests !== undefined ? (
-                    allGuests.map((guest,idx) =>{
-                        return <GuestCard idx={idx} guest={guest} />
-                    })
-                ) :null 
-                }
-            </>
-        )
-        
-    }
-    else if(allMessages){
-        allMessages.map((message) =>{
-            return (
-                <div>
-                    Message: {message.body} 
-                </div>
-            )
-        })
-    }
-    else{
-        return (
-            <div>
-                howdy
-            </div>
-        )
-    }
-    
-}
+export const InfoSilo = ({ allCompanies, allGuests, allMessages }) => {
+  if (allCompanies && allCompanies.length >= 1) {
+    console.log(" all companies ", allCompanies);
+    return (
+      <div className={"infoSilo"}>
+        <>
+          {allCompanies !== undefined
+            ? allCompanies.map((company, idx) => {
+                return <CompanyCard idx={idx} company={company} />;
+              })
+            : null}
+        </>
+      </div>
+    );
+  } 
+  
+  else if (allGuests && allGuests.length >= 1) {
+    console.log("all guests ", allGuests);
+    return (
+      <div className={"infoSilo"}>
+        <>
+          {allGuests !== undefined
+            ? allGuests.map((guest, idx) => {
+                return <GuestCard idx={idx} guest={guest} />;
+              })
+            : null}
+        </>
+      </div>
+    );
+  } else if (allMessages) {
+    allMessages.map((message) => {
+      return <div>Message: {message.body}</div>;
+    });
+  } else {
+    return <div>howdy</div>;
+  }
+};
