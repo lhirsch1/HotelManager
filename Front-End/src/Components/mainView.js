dccdb109245dd1d580c796d0b183099a442fa2a2
lhirsch1 <hirschlukasa@gmail.com>
2021-10-14 16:35:32 -0500

    added logic to filter guests by company

--- Front-End/src/Components/mainView.js
+++ Front-End/src/Components/mainView.js
@@ -8,10 +8,16 @@ import Col from "react-bootstrap/Col";
 export const MainView = (props) => {
   const [companyState, setCompanyState] = useState();
   const [guestState, setGuestState] = useState();
+  const [companyGuestState, setCompanyGuestState] = useState();
   const [messageState, setMessageState] = useState();
+  const [selectedState, setSelectedState] = useState({"company": null, "guest":null});
   const dataCompanies = api.getAll("companies");
   console.log(dataCompanies);
 
+  let guests = [];
+
+  //useeffect fired on first render
+  //sets company state and populates guests array
   useEffect(() => {
     api.getAll("companies").then((results) => {
       console.log("results", results);
@@ -19,25 +25,49 @@ export const MainView = (props) => {
     });
     api.getAll("guests").then((results) => {
       console.log("resultsguest ", results);
-      setGuestState(results);
+    setGuestState(results)
     });
   }, []);
 
-  useEffect(() => {}, [companyState, guestState, messageState]);
+  useEffect(() => {}, [companyState, guestState]);
+
+
+  // this use effect handles when columns are populated
+  useEffect(() => {
+    console.log('selected state changed', selectedState)
+    if(selectedState.company && !selectedState.guest){
+        // console.log('company selected no guests ', selectedState)
+        // console.log('guest list ', guests)
+        let companyGuests = guestState.filter((guest)=> {
+            return guest.reservation.hotel === selectedState.company
+        });
+        console.log('company guests ', companyGuests)
+        setCompanyGuestState(companyGuests)
+     
+
+    }
+    else if(selectedState.company && selectedState.guest){
+        console.log('select populated', selectedState)
+        api.getOne('messages', selectedState.guest)
+    }
+    else{
+        console.log('no selected ', selectedState)
+    }
+  },[selectedState])
 
   return (
     <Container>
       <Row>
         <Col lg={4}>
-            <h2>Click to select a guest</h2>
-          <InfoSilo allCompanies={companyState} />
+            <h2>Click to select a company</h2>
+          <InfoSilo allCompanies={companyState} setSelectedState={setSelectedState} selectedState={selectedState} />
         </Col>
         <Col lg={4}>
-         <h2>Click to select a company</h2>
-          <InfoSilo allGuests={guestState} />
+         <h2>Click to select a guest</h2>
+          <InfoSilo allGuests={companyGuestState} setSelectedState={setSelectedState} />
         </Col>
         <Col lg={4}>
-          <InfoSilo allMessages={messageState} />
+          <InfoSilo allMessages={messageState}/>
         </Col>
       </Row>
     </Container>
