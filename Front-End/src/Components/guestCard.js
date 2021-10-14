eaab6b049e5d7ad280b6f651441915dd441ff28c
lhirsch1 <hirschlukasa@gmail.com>
2021-10-14 16:41:36 -0500

    adding selected state logic to card

--- Front-End/src/Components/guestCard.js
+++ Front-End/src/Components/guestCard.js
@@ -1,12 +1,18 @@
 
 
 
-export const GuestCard = ({key, guest}) => {
+export const GuestCard = ({key, guest, selectedState, setSelectedState}) => {
+
+    const handleClick = (id) => {
+        setSelectedState({...selectedState, "guest":id})
+    
+    }
+    
     
     return(
-        <div className={"card"}>
+        <button  onClick={()=> {handleClick(guest.id)}}>
             <h2>{guest.firstName} {guest.lastName}</h2>
    
-        </div>
+        </button>
     )
 }
