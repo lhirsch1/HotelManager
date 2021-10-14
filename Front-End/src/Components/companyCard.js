6679606b269ba880b30658d321f97c48560c8de6
lhirsch1 <hirschlukasa@gmail.com>
2021-10-14 13:42:24 -0500

    setting up company card

---
+++ Front-End/src/Components/companyCard.js
@@ -0,0 +1,12 @@
+//displays company data
+
+
+export const CompanyCard = ({key, company}) => {
+    
+    return(
+        <>
+            <h2>{props.company} {props.city}</h2>
+            <h3>{props.timezone}</h3>
+        </>
+    )
+}
