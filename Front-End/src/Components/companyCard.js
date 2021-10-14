8c62400b464f192c07f6dd6ae2e8730d4b2c2bd8
lhirsch1 <hirschlukasa@gmail.com>
2021-10-14 16:36:37 -0500

    removing log

--- Front-End/src/Components/companyCard.js
+++ Front-End/src/Components/companyCard.js
@@ -1,14 +1,24 @@
 //displays company data
 
 
-export const CompanyCard = ({idx, company}) => {
-    console.log('key ', idx)
+export const CompanyCard = ({idx, company, setSelectedState, selectedState}) => {
+    // console.log('key ', idx)
+
+    const handleClick = (id) => {
+        setSelectedState({...selectedState, "company":id})
+
+    }
     
     return(
       
-        <div id={company.id} className={"card"}>
+        <button 
+        // className={"card"} 
+        // onclick={()=>setSelectedState({...selectedState, "company": company.id})} 
+        onClick={()=> {handleClick(company.id)}}
+
+        >
             <h2>{company.company} {company.city}</h2>
             <h3>{company.timezone}</h3>
-        </div>
+        </button>
     )
 }
