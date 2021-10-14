7782bf306061af2f54c14410a44659b5a67c83b0
lhirsch1 <hirschlukasa@gmail.com>
2021-10-14 12:23:06 -0500

    creating react app template and starting API routes

---
+++ Front-End/src/Components/api.js
@@ -0,0 +1,37 @@
+
+
+
+
+const api = {
+
+    baseURL: "http://localhost:8080",
+
+    //get all takes in table and returns all results 
+    getAll: async (table) =>{
+        const data = await fetch(`${api.baseURL}/${table}`, {
+            method:"GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            }
+        })
+        return data.json()
+    },
+
+    // post is used to update messages and guests tables
+    post: async (table, obj) => {
+        const data = await fetch(`${api.baseURL}/${table}`, {
+            method:"POST",
+            body: JSON.stringify(obj),
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            }
+        })
+        return data.json()
+    }
+
+
+
+
+}
