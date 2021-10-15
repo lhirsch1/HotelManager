442294433816b1be3c268a366675c57451fa3601
lhirsch1 <hirschlukasa@gmail.com>
2021-10-15 17:20:57 -0500

    fixed userid issue

--- Front-End/src/Components/messageForm.js
+++ Front-End/src/Components/messageForm.js
@@ -1,73 +1,71 @@
-import React, {useEffect, useState} from "react";
-import {TimeConverter} from "./timeConverter";
+import React, { useEffect, useState } from "react";
+import { TimeConverter } from "./timeConverter";
 import { MessageTemplate } from "./messageTemplate";
-import {api} from './api';
+import { api } from "./api";
 
+export const MessageForm = ({ selectedState, setSelectedState, messageState, setMessageState }) => {
+  console.log("all messages form ", messageState);
+  console.log('message form selectedstate', selectedState)
 
+  const [msgType, setMsgType] = useState();
+  const [msgBody, setMsgBody] = useState();
 
+  let daytime = TimeConverter(selectedState.company.timezone);
+  console.log('daytime', daytime)
 
-
-export const MessageForm = ({selectedState, setSelectedState}) => {
-
-    console.log('msg comp', selectedState)
-
-    const [msgType, setMsgType] = useState();
-    const [msgBody, setMsgBody] = useState();
-
-
-    let daytime = TimeConverter(selectedState.company.timezone)
-    
-
-    useEffect(()=>{
+  useEffect(() => {
     let firstName = selectedState.guest.firstName;
     let company = selectedState.company.company;
     let timePhrase = daytime;
-    let roomNumber = selectedState.guest.reservation.roomNumber
+    let roomNumber = selectedState.guest.reservation.roomNumber;
     //messagetype, time, firstname, otel, roomNumber, signOutTime, customMessage
-    let generatedMsg = MessageTemplate(msgType, daytime, firstName, company,roomNumber, 'noon', '' )   
-    setMsgBody(generatedMsg)
-
-    },[msgType])
-
+    let generatedMsg = MessageTemplate(
+      msgType,
+      daytime,
+      firstName,
+      company,
+      roomNumber,
+      "noon",
+      ""
+    );
+    setMsgBody(generatedMsg);
+  }, [msgType]);
 
   const handleSubmit = (e) => {
-      console.log('handleSubmit')
-    e.preventDefault()
-    console.log('event ', msgBody )
-    let msgObj =  {
-        "userId":selectedState.guest.id,
-        "companyId":selectedState.company.id,
-        "type": msgType,
-        "body": msgBody,
-        "dateTime":1634232820
-    }
-
-
-    console.log('msg obj', JSON.stringify(msgObj))
-    api.post(msgObj).then((res)=>{
-        console.log(res)
-    })
-    
-   }
-    return (
-        <div>
-        <form onSubmit={handleSubmit}>
-            <select value={msgType} onChange={e =>{setMsgType(e.target.value)}}>
-                <option>Select message type</option>
-                <option value="welcome">Welcome</option>
-                <option value="signout">Sign Out Reminder</option>
-                <option value="thankyou">Thank You</option>
-                <option value="custom">Custom</option>
-            </select>
-            <input type="submit" />
-        </form>
-        <span>Preview Message : {msgBody}</span>
-        </div>
-
-
-
-    )
-
-
-
-}
+    e.preventDefault();
+    let msgObj = {
+      userId: selectedState.guest.id,
+      companyId: selectedState.company.id,
+      type: msgType,
+      body: msgBody,
+      dateTime: 1634232820,
+    };
+    api.post(msgObj).then((res) => {
+      api.getOne("messages",selectedState.guest.id).then((res) =>{
+          
+            setMessageState(res)
+      })
+      
+    });
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <select
+          value={msgType}
+          onChange={(e) => {
+            setMsgType(e.target.value);
+          }}
+        >
+          <option>Select message type</option>
+          <option value="welcome">Welcome</option>
+          <option value="signout">Sign Out Reminder</option>
+          <option value="thankyou">Thank You</option>
+          <option value="custom">Custom</option>
+        </select>
+        <input type="submit" />
+      </form>
+      <span>Preview Message : {msgBody}</span>
+    </div>
+  );
+};
